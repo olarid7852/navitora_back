9812189bb365697de91e33fcac021a6ff29eaf7c
Ridwan <Olarid7852@gmail.com>
2019-06-10 10:58:11 +0100

    Added requirement files and update settings for heroku

--- car/settings.py
+++ car/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -180,5 +181,12 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASS' : (
 
     ),
-    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
+    # 'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
 }
+
+try:
+    from .local import *
+except ImportError as e:
+    pass
+
+django_heroku.settings(locals())
